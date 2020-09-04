1ac47c0378cda22fccfd66a4dadd85b2539eee49
Paweł 'Andrzej' Pankiewicz <pawel.pankiewicz@xdsa.pl>
2020-09-04 23:23:51 +0200

    Krok 1

--- game/events.py
+++ game/events.py
@@ -2,47 +2,10 @@ import pygame
 
 QUIT = False
 
-UP = False
-DOWN = False
-LEFT = False
-RIGHT = False
-
-SHOOT = False
-
 
 def process_events():
     global QUIT
 
-    global UP
-    global DOWN
-    global LEFT
-    global RIGHT
-
-    global SHOOT
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             QUIT = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                SHOOT = True
-            if event.key == pygame.K_d:
-                RIGHT = True
-            if event.key == pygame.K_a:
-                LEFT = True
-            if event.key == pygame.K_w:
-                UP = True
-            if event.key == pygame.K_s:
-                DOWN = True
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                SHOOT = False
-            if event.key == pygame.K_d:
-                RIGHT = False
-            if event.key == pygame.K_a:
-                LEFT = False
-            if event.key == pygame.K_w:
-                UP = False
-            if event.key == pygame.K_s:
-                DOWN = False
--- game/main.py
+++ game/main.py
@@ -2,22 +2,15 @@ import pygame
 
 import events
 import global_vars
-import loader
-from sprites import bullets
-from sprites import enemy
-from sprites.background import Background
-from sprites.player import Player
+
 
 pygame.init()
-loader.load()
+
 
 pygame.display.set_mode(global_vars.SCREEN_SIZE)
 pygame.display.set_caption("Hell Of Bullets")
 
 clk = pygame.time.Clock()
-player = Player()
-bg = Background()
-enemy_spawn_ctrl = enemy.Controller()
 
 
 fps_target = 30
@@ -31,18 +24,9 @@ while running:
         running = False
 
     # PHYSICS AND CONTROLLER UPDATES
-    player.update(deltaT)
-    bullets.update_all_bullets(deltaT)
-    enemy_spawn_ctrl.update(deltaT)
-    bg.update(deltaT)
 
     # RENDERING
     srf = pygame.display.get_surface()
     srf.fill((0, 0, 0))
-
-    bg.draw()
-    player.draw()
-    bullets.draw_all_bullets()
-    enemy_spawn_ctrl.draw_all_enemies()
 
     pygame.display.update()
